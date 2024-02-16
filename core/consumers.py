dfea39543240b5b49c6a0b066a02ac53bedd4588
sagar02411 <sagarbhanushli@vebuin.com>
2024-02-16 16:41:32 +0530

    [update]: store data in database added

--- core/consumers.py
+++ core/consumers.py
@@ -61,3 +61,22 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
 	async def save_message(self, sender, receiver, message):
 		Message.objects.create(sender=sender, receiver=receiver, content=message)
+
+
+	# 	async def receive(self, text_data):
+	# 		text_data_json = json.loads(text_data)
+	# 		message = text_data_json['message']
+	# 		receiver_username = text_data_json['receiver']
+	# 		receiver = User.objects.get(username=receiver_username)
+	# 		await self.save_message(self.user, receiver, message)
+	# 		await self.send_message(receiver_username, message)
+	
+	# async def send_message(self, receiver_username, message):
+	# 	await self.send(text_data=json.dumps({
+	# 		'sender': self.user.username,
+	# 		'receiver': receiver_username,
+	# 		'message': message
+	# 	}))
+	
+	# async def save_message(self, sender, receiver, message):
+	# 	Message.objects.create(sender=sender, receiver=receiver, content=message)
