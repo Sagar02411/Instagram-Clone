4f05a48c7a327cdea06a014b2658884a82a2697f
sagar02411 <sagarbhanushli@vebuin.com>
2024-02-22 18:11:36 +0530

    [updated]: reactons using ajax

--- core/consumers.py
+++ core/consumers.py
@@ -34,33 +34,43 @@ from django.contrib.auth.models import User
 # 		await self.send(text_data = json.dumps({"message":message ,"username":username}))
 
 
-
- 
 class ChatConsumer(AsyncWebsocketConsumer):
-	async def connect(self):
-		self.user = self.scope['user']
-		await self.accept()
-		
- 
-	async def disconnect(self, close_code):
-		pass
- 
-	async def receive(self, text_data):
-		text_data_json = json.loads(text_data)
-		message = text_data_json['message']
-		receiver_username = text_data_json['receiver']
-		receiver = User.objects.get(username=receiver_username)
-		await self.save_message(self.user, receiver, message)
-		await self.send_message(receiver_username, message)
+    async def connect(self):
+        # Authenticate user
+        if self.scope["user"].is_anonymous:
+            await self.close()
+        else:
+            await self.accept()
 
-	async def send_message(self, receiver_username, message):
-		await self.send(text_data=json.dumps({
-			'sender': self.user.username,
-			'message': message
-		}))
+    async def disconnect(self, close_code):
+        # Clean up on disconnect
+        pass
 
-	async def save_message(self, sender, receiver, message):
-		Message.objects.create(sender=sender, receiver=receiver, content=message)
+    async def receive(self, text_data):
+        data_json = json.loads(text_data)
+        message = data_json['message']
+        receiver_id = data_json['receiver_id']
+        sender = self.scope["user"]
+
+        # Save message to database
+        new_message = Message.objects.create(sender=sender, receiver_id=receiver_id, content=message)
+
+        # Broadcast message to sender and receiver
+        await self.channel_layer.group_send(
+            f"chat_{receiver_id}",
+            {
+                "type": "chat_message",
+                "message": message,
+                "sender_id": sender.id,
+            },
+        )
+
+    async def chat_message(self, event):
+        message = event["message"]
+        sender_id = event["sender_id"]
+
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({"message": message, "sender_id": sender_id}))
 
 
 	# 	async def receive(self, text_data):
